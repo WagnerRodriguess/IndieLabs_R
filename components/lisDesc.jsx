dc6f96fbf000089769c12c1140823d107c0cd708
Lz Gustavo <l.gustavotrabalho@outlook.com>
2025-10-21 09:42:25 -0300

    Mantém app.jsx e adiciona app1.jsx

---
+++ components/lisDesc.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function GameDescription() {
+  return (
+    <div className="text-white max-w-xl">
+      <h2 className="text-5xl font-extrabold mb-4">Celeste</h2>
+      <p className="mb-4 text-justify text-sm sm:text-base leading-relaxed">
+        "Celeste" é um jogo indie lançado em 2018 que rapidamente conquistou a atenção da comunidade de jogadores e críticos. 
+        Desenvolvido pela Maddy Makes Games, o jogo é uma obra-prima de plataforma que combina desafio, narrativa emocional e uma trilha sonora cativante para criar uma experiência única.
+      </p>
+
+      <p className="mb-4 text-justify text-sm sm:text-base leading-relaxed">
+        O enredo de "Celeste" segue a jornada de Madeline, uma jovem mulher que decide escalar a misteriosa montanha Celeste. 
+        Essa jornada não é apenas uma escalada física, mas também uma exploração de seu próprio interior, repleta de metáforas sobre superação de obstáculos e aceitação pessoal.
+      </p>
+
+      <p className="mb-4 text-justify text-sm sm:text-base leading-relaxed">
+        A jogabilidade é o ponto alto de "Celeste". O jogo desafia os jogadores com níveis complexos e cheios de obstáculos, 
+        envolvendo o uso habilidoso das mecânicas de escalada e dash aéreo.
+      </p>
+
+      <p className="text-justify text-sm sm:text-base leading-relaxed">
+        A trilha sonora, composta por Lena Raine, é uma das mais marcantes do gênero indie, complementando a emoção e a imersão do jogo.
+      </p>
+    </div>
+  );
+}
+
+export default GameDescription;
