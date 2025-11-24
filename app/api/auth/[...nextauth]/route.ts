536ea0ccbba1afffea21d20671d84db6db6ebf32
Wagner Rodrigues <109499969+WagnerRodriguess@users.noreply.github.com>
2025-11-24 18:30:47 -0300

    Update nextauth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -38,10 +38,8 @@ export const authOptions: AuthOptions = {
       }
     })
   ],
-  
 
-  secret: "wingman_desenvolvimento_de_sistemas",
-  
+  secret: process.env.NEXTAUTH_SECRET,
 
   debug: process.env.NODE_ENV === 'development',
 
